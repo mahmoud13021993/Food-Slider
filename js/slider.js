930a6e1d78bd779617c74a4f93504f3ae16ab5ab
Mahmoud Khairy <134206930+mahmoud13021993@users.noreply.github.com>
2023-08-10 14:28:20 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -4,11 +4,6 @@ let closeBtn = document.getElementById('closeBtn');
 let rightBtn = document.getElementById('rightBtn');
 let fixedBox = document.getElementById('fixedBox');
 let boxShow = document.getElementById('boxShow');
-
-// get elemnts 
-// click show slider 
-// click close hide slider
-// the same img clicked
 let currentIndex;
 for(let i=0; i<img.length;i++) {
     img[i].addEventListener('click',(e) => {
@@ -29,32 +24,6 @@ function closeIcon() {
 closeBtn.addEventListener('click', function() {
     closeIcon()
 })
-
-// function nextImg() {
-//     currentIndex++
-//     console.log(currentIndex)
-//     if(currentIndex === img.length) {
-//         currentIndex = 0;
-//         console.log(currentIndex)
-//     }
-//     let nextImg = img[currentIndex];
-//     let imgSrc = nextImg.getAttribute('src');
-//     boxShow.style.backgroundImage = `url(${imgSrc})`
-
-// }
-// function prevImg() {
-//     currentIndex--
-//     console.log(currentIndex)
-//     if(currentIndex === 0) {
-//         currentIndex = img.length-1;
-//         console.log(currentIndex)
-//     }
-//     let nextImg = img[currentIndex];
-//     let imgSrc = nextImg.getAttribute('src');
-//     boxShow.style.backgroundImage = `url(${imgSrc})`
-
-// }
-
 
 
 function slide(step) {
